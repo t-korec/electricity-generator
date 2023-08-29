7dddb5a2744e5e8b17f60d2f12b0ead3ef1be3f7
tkorec <tkorec@teamworks.com>
2023-08-29 08:19:09 +0200

    refactor: use later redux fn to create stpre

--- src/redux/Store.tsx
+++ src/redux/Store.tsx
@@ -1,8 +1,11 @@
-import { applyMiddleware, createStore } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from './slices/CounterSlice';
 import thunk from 'redux-thunk';
 
-const store = createStore(counterReducer, applyMiddleware(thunk));
+const store = configureStore({
+  reducer: counterReducer,
+  middleware: [thunk],
+});
 
 store.subscribe(() => {
   localStorage.setItem('genState', JSON.stringify(store.getState().data));
